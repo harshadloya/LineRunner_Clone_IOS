4149dfd93887e10c5bf38f7c4517bdc25f4446ad
Harshad Loya <hloya1@wp_fr_03.pods.bu.int>
2017-03-13 16:59:42 -0400

    Basic physics and images added, runner jumps on click

--- p04-loya/GameScene.swift
+++ p04-loya/GameScene.swift
@@ -9,68 +9,64 @@
 import SpriteKit
 import GameplayKit
 
+
+struct PCat {
+    
+    static let Runner : UInt32 = 0x1 << 1
+    static let Ground : UInt32 = 0x1 << 2
+    //static let Runner : UInt32 = 0x1 << 1
+}
+
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     var Ground = SKSpriteNode()
+    var Runner = SKSpriteNode()
+    var Background = SKSpriteNode()
+    
     
     override func didMove(to view: SKView) {
         
         Ground = SKSpriteNode(imageNamed: "ground")
-        Ground.setScale(0.5)
+        Ground.setScale(0.62)
         Ground.position = CGPoint(x: self.frame.size.width / 2, y: 0 + Ground.frame.height / 2)
         
+        Ground.physicsBody = SKPhysicsBody(rectangleOf: Ground.size)
+        Ground.physicsBody?.categoryBitMask = PCat.Ground
+        Ground.physicsBody?.collisionBitMask = PCat.Runner
+        Ground.physicsBody?.contactTestBitMask = PCat.Runner
+        Ground.physicsBody?.affectedByGravity = false
+        Ground.physicsBody?.isDynamic = false
+        Ground.zPosition = 3
         self.addChild(Ground)
         
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
+        Runner = SKSpriteNode(imageNamed: "ca")
+        Runner.size = CGSize (width: 60, height: 70)
+        Runner.position = CGPoint(x: self.frame.size.width / 2, y: 0 + self.frame.height / 2)
+        
+        Runner.physicsBody = SKPhysicsBody(circleOfRadius: Runner.frame.height / 2)
+        Runner.physicsBody?.categoryBitMask = PCat.Runner
+        Runner.physicsBody?.collisionBitMask = PCat.Ground
+        Runner.physicsBody?.contactTestBitMask = PCat.Ground
+        Runner.physicsBody?.affectedByGravity = true
+        Runner.physicsBody?.isDynamic = true
+        Runner.zPosition = 2
+        self.addChild(Runner)
+        
+        Background = SKSpriteNode(imageNamed: "bg")
+        Background.setScale(0.45)
+        Background.position = CGPoint(x: 0, y: self.frame.height)
+        Background.zPosition = 1
+        self.addChild(Background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        Runner.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        Runner.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60))
+        
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
